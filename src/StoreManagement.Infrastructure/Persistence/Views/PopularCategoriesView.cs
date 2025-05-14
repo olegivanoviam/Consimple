113d992686a52a1a1f0a1c00d4de5dad3ec35526
Oleh Ivanov <olegivanoviam@gmail.com>
2025-05-14 04:45:28 +0300

    refactor: remove Views folder from Persistence as we use query-specific view models

--- src/StoreManagement.Infrastructure/Persistence/Views/PopularCategoriesView.cs
+++
@@ -1,8 +0,0 @@
-namespace StoreManagement.Infrastructure.Persistence.Views;
-
-public class PopularCategoriesView
-{
-    public int CategoryId { get; set; }
-    public string CategoryName { get; set; } = string.Empty;
-    public int TotalUnits { get; set; }
-} 
